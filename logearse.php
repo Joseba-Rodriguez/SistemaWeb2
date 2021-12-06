607b73d28a3ce9a82cffa8e6068187899cecb2a0
joseba-rodriguez <joseba.rodriguez01@gmail.com>
2021-12-06 19:57:25 +0100

    CAmbios en timeout

--- logearse.php
+++ logearse.php
@@ -12,6 +12,7 @@
     
     $email = $_POST["email"];
     $_SESSION['correo'] = $email;
+    $_SESSION['correo_time'] = time();
     $password = $_POST["password"];
     
     $instruccion_SQL= $connection->prepare("SELECT * FROM tabla WHERE email=?");
@@ -130,5 +131,7 @@
         <p class="copyright">GaJo © 2021</p>
     </footer>
 </div>
+<meta http-equiv="refresh" content="10;url=index.html" />
+
 </body>
 </html> 
