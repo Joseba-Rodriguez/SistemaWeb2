b91c7f38bfd8bf796762e557f2bfe4ed9365652d
GaizkaAlabort <gaizkalabortupv@gmail.com>
2021-12-06 20:43:08 +0100

    actualizacion

--- comprobarCoche.php
+++ comprobarCoche.php
@@ -20,14 +20,21 @@
     
     $_SESSION['matricula'] = $matricula;
     
-    $instruccion_SQL="SELECT * FROM tablacoches WHERE matricula='$matricula'";
+    $instruccion_SQL= $connection->prepare("SELECT * FROM tablacoches WHERE matricula = ?");
 
-    $resultado = mysqli_query($connection, $instruccion_SQL) or die (mysqli_error($connection));
+    $instruccion_SQL->bind_param("s",$matricula);
+
+    $instruccion_SQL->execute();
+
+    $resultado = $instruccion_SQL->get_result();
+    
+    #$instruccion_SQL="SELECT * FROM tablacoches WHERE matricula='$matricula'";
+    #$resultado = mysqli_query($connection, $instruccion_SQL) or die (mysqli_error($connection));
     
     if(!$resultado){
         echo"Hubo Algun Error";
     }else{
-        if($resultado->num_rows == 1){
+        if($resultado->num_rows== 1){
                 echo"<script>window.location='cambiarDatos.php'</script>";
         } else {
             echo"<script>alert('La matricula no existe'); window.location='mostrarCochesQueAlquilan.php'</script>";
